5ae1756ff4adaf4e4c33c967d3154d5911205488
Ilya S <ilya.s@fmlht.com>
2019-04-16 14:30:37 +0200

    Upload

---
+++ Tween/Editor/TweenEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace FMLHT {
+
+public class TweenEditor : Editor
+{
+    [MenuItem("FMLHT/Utilities/Tween/Add to scene")]
+    public static void AddPrefab() {
+        if (Editor.FindObjectOfType<Tween>() == null) {
+            var core = GameObject.Find("Core");
+            if (core == null) {
+                core = new GameObject();
+                core.name = "Core";
+            }
+            var utilities = core.transform.Find("Utilities");
+            if (utilities == null) {
+                utilities = new GameObject().transform;
+                utilities.name = "Utilities";
+            }
+            utilities.SetParent(core.transform);
+            utilities.gameObject.AddComponent<Tween>();
+        } else {
+            Debug.Log("There is already one Tween in this scene!");
+        }
+    }
+}
+
+}
